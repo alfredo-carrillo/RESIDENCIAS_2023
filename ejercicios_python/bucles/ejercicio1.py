185fd636bf616277740d059d28d6106b3964d405
Christopher Calix <ccalix@residentes1.oficina.yezdo.com>
2023-10-09 17:37:34 -0600

    Eliminando carpeta erronea

--- ejercicios_python/bucles/ejercicio1.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 1
-
-#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
-
-palabra  = input("Ingresa una palabra: ")
-
-for i in range(10):
-    print(palabra)
--- ejercicios_python/tipos_datos_simples/ejercicio8.py
+++
@@ -1,9 +0,0 @@
-
-n1 = input("¿Ingresa el numero 1? ")
-
-n2 = input("¿Ingresa el numero 2?")
-
-cociente = int(n1)/int(n2)
-residuo =  int(n1)%int(n2)
-
-print("la operscion entre : ", str(n1) +" y ", str(n2) +" da un cociente de :", str(cociente) +" y un residuo de", str(residuo))
