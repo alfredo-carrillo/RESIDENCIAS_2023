8671b610b51ff69eb9e42a4811bc8e679287da4f
Christopher Calix <ccalix@residentes1.oficina.yezdo.com>
2023-10-09 18:02:55 -0600

    delete test

--- ejercicios_python/diccionarios/ejercicio1.py
+++
@@ -1,9 +0,0 @@
-#Ejercicio 1
-#Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, 
-#pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
-
-diccionario = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
-
-divisa = input(f"selecciona alguna de las divisas {diccionario}: ")
-
-if divisa ==  
