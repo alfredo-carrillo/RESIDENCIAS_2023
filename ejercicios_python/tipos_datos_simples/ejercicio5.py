abb66ea3cdf1f9535f8aeba8e06e1f8aee89f66e
Christopher Calix <ccalix@residentes1.oficina.yezdo.com>
2023-11-13 14:57:20 -0600

    DELETE:ejercicios_python

--- ejercicios_python/tipos_datos_simples/ejercicio5.py
+++
@@ -1,9 +0,0 @@
-
-user = "Valentin Elizalde"
-print("Hola", user +", 12cuantas horas trabjaó el día de hoy?")
-horas_laboradas = input()
-print("Cuanto es el coste por tu hora de trabajo?")
-coste_hrs = input()
-paga = int(horas_laboradas) * int(coste_hrs)
-
-print("Muy bien", user +" tu paga es de :", paga)
